3d9e741cc3cdf0ee5da799867ccf3ecc4a0eea3b
Daniel Jones <daniel.jones@engineerbetter.com>
2020-03-25 12:48:27 +0000

    run hugo copying in stuff from sibling dir

---
+++ run.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+: "${TUTORIAL:?TUTORIAL env var must specify name of sibling directory, eg 'trycf'}"
+
+set -euo pipefail
+
+which fswatch
+mkdir -p themes
+
+SRC_CONFIG_PATH=../tutorials/ci/tutorial/hugo-config.yml
+SRC_THEME_PATH=../hugo-whisper-theme
+SRC_CONTENT_PATH=../${TUTORIAL}/content
+
+trap "exit" INT
+
+(sleep 2 && hugo serve -D) &
+
+while true; do
+  rsync -avz ${SRC_CONTENT_PATH} .
+  rsync -avz ${SRC_THEME_PATH} themes/
+  rsync -v ${SRC_CONFIG_PATH} config.yml
+
+  fswatch --one-event \
+    ${SRC_THEME_PATH} \
+    ${SRC_CONFIG_PATH} \
+    ${SRC_CONTENT_PATH}
+done
